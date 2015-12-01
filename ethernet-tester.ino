8cdefd04c41875994caca3229ea5b439151daa6a
Francois Best <francoisbest@gmail.com>
2015-12-01 12:14:30 +0100

    Fixed pin mapping & ground reference.

--- ethernet-tester.ino
+++ ethernet-tester.ino
@@ -1,21 +1,65 @@
 #include <Bounce.h>
 
 // -----------------------------------------------------------------------------
-// Hardware Mapping
+// Hardware Mapping - Arduino Leonardo
 
-static const byte sPins[8] = 
+template<int Pin>
+class ToggleButton : public Bounce
+{
+public:
+    static const int sDebouncingTimeMs = 10;
+
+public:
+    inline ToggleButton()
+        : Bounce(Pin, sDebouncingTimeMs)
+        , mState(false)
+    {
+
+    }
+
+public:
+    static inline void init()
+    {
+        pinMode(Pin, INPUT_PULLUP);
+    }
+
+    inline bool process()
+    {
+        const bool status = update();
+
+        if (fallingEdge())
+        {
+            mState = true; // Pressed
+        }
+        else if (risingEdge())
+        {
+            mState = false; // Released
+        }
+        if (mState != mPreviousState)
+        {
+            mPreviousState = mState;
+            return mState;
+        }
+        return false;
+    }
+private:
+    bool mState;
+    bool mPreviousState;
+};
+
+static const byte sOutputPins[8] =
 {
     0, 1, 2, 3, 4, 5, 6, 7
 };
 
-static const byte sPinMode = 14; // A0
-static const byte sPinIncr = 15; // A1
-static const byte sPinDecr = 16; // A2
+static const byte sModePin = 20; // A2
+static const byte sDecrPin = 21; // A3
+static const byte sIncrPin = 22; // A4
+static const byte sGndPin  = 23; // A5
 
-static const unsigned long sDebouncingTime = 10; // ms
-Bounce sModeButton(sPinMode, sDebouncingTime);
-Bounce sIncrButton(sPinIncr, sDebouncingTime);
-Bounce sDecrButton(sPinDecr, sDebouncingTime);
+ToggleButton<sModePin> sModeButton;
+ToggleButton<sIncrPin> sIncrButton;
+ToggleButton<sDecrPin> sDecrButton;
 
 // -----------------------------------------------------------------------------
 
@@ -42,10 +86,11 @@ bool yield(unsigned inTime)
 
     for (unsigned i = 0; i < numIterations; ++i)
     {
-        sModeButton.update();
-        if (sModeButton.fallingEdge())
+        if (sModeButton.process())
         {
             sRunningMode = !sRunningMode;
+            Serial.print("Running Mode: ");
+            Serial.println(sRunningMode ? "manual" : "automatic");
             return true;
         }
         delay(sSleepTime);
@@ -59,8 +104,16 @@ void reset()
 {
     for (byte i = 0; i < 8; ++i)
     {
-        digitalWrite(sPins[i], LOW);
+        digitalWrite(sOutputPins[i], LOW);
     }
+}
+
+void updatePins(byte inPrevious)
+{
+    digitalWrite(sOutputPins[inPrevious],   LOW);
+    digitalWrite(sOutputPins[sCurrentStep], HIGH);
+    Serial.print("Pin ");
+    Serial.println(sCurrentStep + 1);
 }
 
 // -----------------------------------------------------------------------------
@@ -69,10 +122,9 @@ void runAuto()
 {
     while (true)
     {
-        const unsigned current = sCurrentStep;
+        const unsigned previous = sCurrentStep;
         sCurrentStep = (sCurrentStep + 1) & 0x7;
-        digitalWrite(sPins[current],        LOW);
-        digitalWrite(sPins[sCurrentStep],   HIGH);
+        updatePins(previous);
         if (yield(1000))
         {
             reset();
@@ -85,21 +137,19 @@ void runManual()
 {
     while (true)
     {
-        const unsigned current = sCurrentStep;
-        sIncrButton.update();
-        sDecrButton.update();
+        const unsigned previous = sCurrentStep;
+        const bool incr = sIncrButton.process();
+        const bool decr = sDecrButton.process();
 
-        if (sIncrButton.fallingEdge())
+        if (incr)
         {
             sCurrentStep = (sCurrentStep + 1) & 0x7;
-            digitalWrite(sPins[current],        LOW);
-            digitalWrite(sPins[sCurrentStep],   HIGH);
+            updatePins(previous);
         }
-        else if (sDecrButton.fallingEdge())
+        else if (decr)
         {
             sCurrentStep = (sCurrentStep - 1) & 0x7;
-            digitalWrite(sPins[current],        LOW);
-            digitalWrite(sPins[sCurrentStep],   HIGH);
+            updatePins(previous);
         }
 
         if (yield(0))
@@ -114,14 +164,19 @@ void runManual()
 
 void setup()
 {
-    pinMode(sPinMode, INPUT_PULLUP);
-    pinMode(sPinIncr, INPUT_PULLUP);
-    pinMode(sPinDecr, INPUT_PULLUP);
+    sModeButton.init();
+    sIncrButton.init();
+    sDecrButton.init();
+    pinMode(sGndPin, OUTPUT); // GND reference
 
     for (byte i = 0; i < 8; ++i)
     {
-        pinMode(sPins[i], OUTPUT);
+        pinMode(sOutputPins[i], OUTPUT);
     }
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.print("Ready. Running mode: ");
+    Serial.println(sRunningMode ? "manual" : "automatic");
 }
 
 void loop()
